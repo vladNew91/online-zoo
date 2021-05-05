c1a47e1a147498b2e044b90f8d6dedb88ad8d9f8
vladNew91 <vladnew91@gmail.com>
2021-05-06 00:52:16 +0300

    feat: add listener onmouseleave on map's parent

--- pages/map/main.js
+++ pages/map/main.js
@@ -1,108 +1,3 @@
-// //map
-// const mapImage = document.querySelector('.world-map');
-// const wrapper = document.querySelector('main');
-// const headerElem = document.querySelector('.header');
-// const footerElem = document.querySelector('footer');
-// const zoomInButton = document.querySelector('.plus');
-// const zoomOutButton = document.querySelector('.minus');
-
-// let topIndent = 0;
-// let leftIndent = 0;
-
-// const calculateCoords = (e, elem) => {
-//   var box = elem.getBoundingClientRect();
-//   topIndent = e.pageY - box.top + pageYOffset;
-//   leftIndent = e.pageX - box.left + pageXOffset;
-// }
-
-// const moveAt = (e) => {
-//   if (mapImage.style.position !== "absolute") {mapImage.style.position = "absolute";}
-//   mapImage.style.left = e.pageX - leftIndent + 'px';
-//   if (e.pageX >= wrapper.offsetWidth) {
-//     stopDrag();
-//   } else if (e.pageX <= 0) {
-//     stopDrag();
-//   }
-//   mapImage.style.top = e.pageY - (200 - pageYOffset) - topIndent + 'px';
-// }
-
-// const stopDrag = () => {
-//   document.removeEventListener('mousemove', moveAt);
-//   mapImage.removeEventListener('mouseup', stopDrag);
-// }
-
-// //mousedown
-// mapImage.addEventListener('mousedown', (e) => {
-
-//   if (mapImage.width <= wrapper.offsetWidth && mapImage.height <= wrapper.offsetHeight) {
-//     return;
-//   }
-
-//   calculateCoords(e, mapImage);
-//   moveAt(e);
-
-//   document.addEventListener('mousemove', moveAt);
-//   mapImage.addEventListener('mouseup', stopDrag);
-// });
-
-
-
-// mapImage.ondragstart = function() {
-//   return false;
-// };
-
-// headerElem.addEventListener('mouseenter', stopDrag);
-// footerElem.addEventListener('mouseenter', stopDrag);
-
-// zoomInButton.addEventListener('click', () => {
-//   if (mapImage.width <= wrapper.offsetWidth * 2) {
-//     if (mapImage.style.position !== "absolute") {mapImage.style.position = "absolute";}
-//     const prevWidth = mapImage.width;
-//     const prevHeight = mapImage.height;
-//     mapImage.style.width = `${mapImage.width * 1.25}px`;
-//     mapImage.style.height = "auto";
-//     const nextWidth = mapImage.width;
-//     const nextHeight = mapImage.height;
-//     const topPos = mapImage.offsetTop || 0;
-//     const leftPos = mapImage.offsetLeft || 0;
-
-
-//     mapImage.style.left = `${leftPos - ((nextWidth - prevWidth) / 2)}px`;
-//     mapImage.style.top = `${topPos - ((nextHeight - prevHeight) / 2)}px`;
-//   }
-// });
-
-// zoomOutButton.addEventListener('click', () => {
-//   if (mapImage.width >= wrapper.offsetWidth || mapImage.height >= wrapper.offsetHeight) {
-//     if (mapImage.style.position !== "absolute") {mapImage.style.position = "absolute";}
-//     const prevWidth = mapImage.width;
-//     const prevHeight = mapImage.height;
-//     mapImage.style.width = `${mapImage.width / 1.25}px`;
-//     mapImage.style.height = "auto";
-//     const nextWidth = mapImage.width;
-//     const nextHeight = mapImage.height;
-//     const topPos = mapImage.offsetTop || 0;
-//     const leftPos = mapImage.offsetLeft || 0;
-
-
-//     mapImage.style.left = `${leftPos + ((prevWidth - nextWidth) / 2)}px`;
-//     mapImage.style.top = `${topPos + ((prevHeight - nextHeight) / 2)}px`;
-
-//     if (mapImage.width <= wrapper.offsetWidth && mapImage.height <= wrapper.offsetHeight) {
-//       mapImage.style.width = `${wrapper.offsetWidth}px`;
-//       mapImage.style.height = "auto";
-//       mapImage.style.top = `${(wrapper.offsetHeight - mapImage.height) / 2}px`;
-//       mapImage.style.left = '0px';
-//       if (mapImage.height >= wrapper.offsetHeight) {
-//         mapImage.style.height = `${wrapper.offsetHeight}px`;
-//         mapImage.style.width = 'auto';
-//         mapImage.style.top = '0px';
-//         mapImage.style.left = `${(wrapper.offsetWidth - mapImage.width) / 2}px`;
-//       }
-//     }
-//   }
-// });
-
 //modal window 1,2
 const modal = document.querySelector(".modal");
 const modal1 = document.querySelector(".modal-1");
@@ -196,8 +91,9 @@ const zoomIn = () => {
   let currHeightItems = mapItems.clientHeight;
 
   if (currWidthImage > 2000) return;
-
+  //map width
   mapImage.style.width = (currWidthImage * 1.15) + "px";
+  //map items width, height
   mapItems.style.width = (currWidthItems * 1.15) + "px";
   mapItems.style.height = (currHeightItems * 1.15) + "px";
 };
@@ -224,6 +120,7 @@ const zoomOut = () => {
 //drag & drop
 const getCoords = (elem) => {
   let box = elem.getBoundingClientRect();
+
   return {
     top: box.top + pageYOffset,
     left: box.left + pageXOffset
@@ -238,7 +135,7 @@ mapImage.addEventListener("mousedown", (e) => {
   const moveAt = (e) => {
     mapImage.style.left = e.pageX - shiftX + 'px';
     mapImage.style.top = e.pageY - shiftY + 'px';
-  }
+  };
 
   moveAt(e);
 
@@ -248,14 +145,13 @@ mapImage.addEventListener("mousedown", (e) => {
 
   mapImage.onmouseup = () => {
     document.onmousemove = null;
-    mapImage.onmouseup = null;
+  };
+
+  mapParent.onmouseleave = () => {
+    document.onmousemove = null;
+  };
+  
+  mapImage.ondragstart = () => {
+    return;
   };
 });
-
-mapParent.onmouseover = () => {
-  mapImage = null;
-};
-
-mapImage.ondragstart = () => {
-  return;
-};
